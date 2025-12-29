ff04f970b1755b017c40c059d6a12eb3bf6c4280
joelarula <joel.arula@gmail.com>
2025-12-29 15:12:26 +0200

    restore snakle game

--- minigames/snake/src/model.rs
+++ minigames/snake/src/model.rs
@@ -3,30 +3,28 @@ use common::path::UniversalPath;
 use serde::{Deserialize, Serialize};
 
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct Hunter {
-    uuid: Uuid,
-    actor: Uuid,   
-    score: u32,
-    hits: u32,
-}
-
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-struct Target {
+pub struct Snake {
     uuid: Uuid,
     actor: Uuid,
-    lives: u8,   
-    reward: u32,
     path: UniversalPath,
+    color: Color,
+    lives: u8,
+    score: u32,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-struct HunterGame {
-    game: Uuid,
-    controller: Uuid,
-    hunters: Vec<Hunter>,   
-    snakes: Vec<Snake>,
+pub struct Snak {
+    uuid: Uuid,
+    name: String,
+    actor: Uuid,
+    path: UniversalPath,
+    reward: u32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SnakeGame {
+    snakes: Vec<Snake>,   
     snaks: Vec<Snak>,
-    targets: Vec<Target>,  
 }
